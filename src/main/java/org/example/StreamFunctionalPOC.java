33a10ae7674810ffc04786ee880d353f728192fd
Andrei Lima <andrei.lima75@gmail.com>
2026-05-01 18:23:10 -0300

    StreamFunctionalPOC

--- src/main/java/org/example/StreamFunctionalPOC.java
+++ src/main/java/org/example/StreamFunctionalPOC.java
@@ -2,9 +2,12 @@ package org.example;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 public class StreamFunctionalPOC {
@@ -25,5 +28,20 @@ public class StreamFunctionalPOC {
         Consumer<Integer> imprimirDobro = n -> System.out.println("Dobro de " + n + " = " + (n * 2));
 
         numeros.forEach(imprimirDobro);
+
+        UnaryOperator<Integer> quadrado = x -> x * x;
+
+        BinaryOperator<Integer> soma = Integer::sum;
+
+        List<Integer> quadrados = numeros.stream()
+                .map(quadrado)
+                .toList();
+
+        Optional<Integer> somaDosQuadrados = quadrados.stream()
+                .reduce(soma);
+
+        somaDosQuadrados.ifPresent(resultado ->
+                System.out.println("Soma dos quadrados = " + resultado)
+        );
     }
 }
